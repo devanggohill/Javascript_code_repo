b118d049750279a11e909b2720211bd08bf869b2
Devang Gohil <devangg961@gmail.com>
2026-06-11 15:26:57 +0530

    Delete javascriptbasics/varbasics.js

--- javascriptbasics/varbasics.js
+++
@@ -1,7 +0,0 @@
-var account_id = 932400
-let account_email ="devanggohil@gmail.com"
-var account_password ="devashet@123"
-account_city ="Jaipur"
-
-console.log(account_id);
-console.table([account_id,account_email,account_password,account_city])
