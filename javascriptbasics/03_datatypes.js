a333fe94e8d22f90eff577cafd3c8cc23ca637c7
Devang Gohil <devangg961@gmail.com>
2026-06-11 09:48:59 +0000

    changing file names

---
+++ javascriptbasics/03_datatypes.js
@@ -0,0 +1,19 @@
+console.log(3+3)
+console.log("Dinesh")
+
+let name = "Pratibha"
+let age = 43
+let isboolean = false
+
+console.log(typeof age)
+console.log(typeof name )
+
+/* number
+boolean 
+bigint
+string 
+undefined 
+null 
+symbol 
+*/ 
+
